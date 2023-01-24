d69af9e02b93da642043684b359b088e4b96c956
George McEntegart <geerodge@gmail.com>
2023-01-24 08:23:55 +0000

    Testing email submission form

--- config/shortcodes/index.js
+++ config/shortcodes/index.js
@@ -1,6 +1,7 @@
 const imageShortcodePlaceholder = require('./imagePlaceholder');
 const includeRaw = require('./includeRaw');
 const liteYoutube = require('./youtube-lite');
+
 module.exports = {
   imageShortcodePlaceholder,
   includeRaw,
--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -64,3 +64,85 @@ services.forEach(services => {
     }
   };
 });
+
+// /**
+//  * Helper function for POSTing data as JSON with fetch.
+//  *
+//  * @param {Object} options
+//  * @param {string} options.url - URL to POST data to
+//  * @param {FormData} options.formData - `FormData` instance
+//  * @return {Object} - Response body from URL that was POSTed to
+//  */
+// async function postFormDataAsJson({ url, formData }) {
+// 	const plainFormData = Object.fromEntries(formData.entries());
+// 	const formDataJsonString = JSON.stringify(plainFormData);
+
+// 	const fetchOptions = {
+// 		method: "POST",
+// 		headers: {
+// 			"Content-Type": "application/json",
+// 			Accept: "application/json",
+// 		},
+// 		body: formDataJsonString,
+// 	};
+
+// 	const response = await fetch(url, fetchOptions);
+
+// 	if (!response.ok) {
+// 		const errorMessage = await response.text();
+// 		throw new Error(errorMessage);
+// 	}
+
+// 	return response.json();
+// }
+
+// /**
+//  * Event handler for a form submit event.
+//  *
+//  * @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+//  *
+//  * @param {SubmitEvent} event
+//  */
+// async function handleFormSubmit(event) {
+// 	event.preventDefault();
+
+// 	const form = event.currentTarget;
+// 	const url = form.action;
+
+// 	try {
+// 		const formData = new FormData(form);
+// 		const responseData = await postFormDataAsJson({ url, formData });
+
+// 		console.log({ responseData });
+//     await EleventyFetch.post("/.netlify/functions/add-email-subscriber", responseData);
+//       alert("Contact details were added successfully");
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// }
+
+// This needs to be added to above or something
+
+const axios = require('axios');
+
+async function handleFormSubmit(event) {
+
+  event.preventDefault();
+  const email = event.currentTarget.email.value;
+  console.log(email);
+
+  try {
+      const payload = {
+          email: email
+      };
+
+  await axios.post("/.netlify/functions/add-email-subscriber", payload);
+      alert("Contact details were added successfully");
+  } catch (error) {
+      alert(error.message);
+  }
+};
+
+const newsletter = document.getElementById("primo-newsletter");
+newsletter.addEventListener("submit", handleFormSubmit);
+
